20d6de8639a3b210cdb8d6d5a4fd01567b43d6e6
Zeyu Liu <zliu@ellisdon.com>
2020-11-01 20:27:22 -0500

    finish task unit tests

--- src/__tests__/tasks.test.js
+++ src/__tests__/tasks.test.js
@@ -1,7 +1,14 @@
 const request = require("supertest");
 const app = require("../server/app");
 const Task = require("../models/Tasks");
-const { testUser, testUserId, seedDB } = require("../db/fixtures/db");
+const {
+  testUser,
+  testUserId,
+  seedDB,
+  testUserTwo,
+  testUserTwoId,
+  taskOne
+} = require("../db/fixtures/db");
 
 beforeEach(seedDB);
 
@@ -17,13 +24,32 @@ test("should be able to create a task for this user", async () => {
     .send(data)
     .expect(200);
 
-
   const task = await Task.findById(res.body._id);
-  expect(task).not.toBeNull()
+  expect(task).not.toBeNull();
 });
 
-test("should get all tasks for this user", () => {});
+test("should get all tasks for this user", async () => {
+  const resOne = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${testUser.tokens[0].token}`)
+    .expect(200);
+  expect(resOne.body.length).toBe(1);
+
+  const resTwo = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${testUserTwo.tokens[0].token}`)
+    .expect(200);
+  expect(resTwo.body.length).toBe(2);
+});
+
+test("should not be able to delete task for other user", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${testUser.tokens[0].token}`)
+    .expect(404);
+
+  let task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
+});
 
 test("should be able to update task for this user", () => {});
-
-test("should not be able to update task for other user", () => {});
--- src/db/fixtures/db.js
+++ src/db/fixtures/db.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/Users");
-const testUserId = new mongoose.Types.ObjectId();
+const Task = require("../../models/Tasks");
 
+const testUserId = new mongoose.Types.ObjectId();
 const testUser = {
   _id: testUserId,
   name: "John Cena",
@@ -16,13 +17,55 @@ const testUser = {
   ]
 };
 
+const testUserTwoId = new mongoose.Types.ObjectId();
+const testUserTwo = {
+  _id: testUserTwoId,
+  name: "Dime Cena",
+  email: "Dime@cena.com",
+  password: "Qwjqyx93",
+  tokens: [
+    {
+      token: jwt.sign({ _id: testUserTwoId }, process.env.JWT_TOKEN_SECRET)
+    }
+  ]
+};
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  task_desc: "task one",
+  completed: false,
+  owner: testUserTwoId
+};
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  task_desc: "task two",
+  completed: false,
+  owner: testUserTwoId
+};
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  task_desc: "task three",
+  completed: false,
+  owner: testUserId
+};
+
 const seedDB = async () => {
   await User.deleteMany();
+  await Task.deleteMany();
   await new User(testUser).save();
+  await new User(testUserTwo).save();
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
   testUser,
+  testUserTwo,
   testUserId,
-  seedDB
+  testUserTwoId,
+  seedDB,
+  taskOne
 };
